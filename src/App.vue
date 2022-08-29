64cc69d66bc3ad9f4515e30062cf85d446000589
DJ089 <dongju089@gmail.com>
2022-08-29 13:36:17 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <h1 class="text-center">todo app</h1>
+    <input 
+      type="text" 
+      class="w-100 p-2" 
+      placeholder="Type todo"
+      @keyup.enter="addTodo"
+      v-model="todoText"
+    >
+    <hr>
+    <Todocomponent
+  
+      v-for="todo in todos"
+      :key="todo.id"
+      :todo="todo"
+      @toggle-checkbox="toggleCheckbox"
+      @click-delete="clickDelete"
+      />
+
+
+
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Todocomponent from '@/components/Todocomponent.vue';
+
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    components: { 
+      Todocomponent 
+    },
+    data() {
+      return{
+        todoText: '',
+        todos: [
+          {id: 1, text: 'buy a car',checked:false},
+          {id:2, text: 'play a game',checked:false},
+        ]
+      }
+    },
+    methods: {
+      addTodo(e) {
+          this.todos.push({
+            id: Math.random(),
+            text: e.target.value,
+            checked: false
+          });
+          this.todoText='';
+
+      },
+      toggleCheckbox({id,checked}){
+        const index = this.todos.findIndex(todo => {
+          return todo.id === id;
+
+        });
+        this.todos[index].checked = checked;
+
+      },
+      clickDelete(id){
+        const index = this.todos.findIndex(todo => {
+          return todo.id === id;
+
+        });
+        this.todos.splice(index,1);
+        
+
+      }
+    }
+
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
